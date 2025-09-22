241b070abc1f21bc71d4bc4c9aaf1ec22a2d87af
sato_issei <sato.issei@alhinc.jp>
2025-09-22 14:03:01 +0900

    formとentityを実装

--- src/main/java/com/example/SMG_BBS/controller/form/CommentForm.java
+++ src/main/java/com/example/SMG_BBS/controller/form/CommentForm.java
@@ -1,4 +1,24 @@
 package com.example.SMG_BBS.controller.form;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
 public class CommentForm {
+
+    private int id;
+
+    private String text;
+
+    private int userId;
+
+    private int messageId;
+
+    private Timestamp createdDate;
+
+    private Timestamp updatedDate;
+
 }
--- src/main/java/com/example/SMG_BBS/controller/form/MessageForm.java
+++ src/main/java/com/example/SMG_BBS/controller/form/MessageForm.java
@@ -1,4 +1,26 @@
 package com.example.SMG_BBS.controller.form;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
 public class MessageForm {
+
+    private int id;
+
+    private String title;
+
+    private String text;
+
+    private String category;
+
+    private int userId;
+
+    private Timestamp createdDate;
+
+    private Timestamp updatedDate;
+
 }
