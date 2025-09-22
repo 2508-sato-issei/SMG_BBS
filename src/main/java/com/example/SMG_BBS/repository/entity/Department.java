a6b6443ea05d5d07565b6c600eb7f67dc807f819
shiraiwa_kazuki <shiraiwa.kazuki@alhinc.jp>
2025-09-22 14:02:45 +0900

    Entityを実装

--- src/main/java/com/example/SMG_BBS/repository/entity/Department.java
+++ src/main/java/com/example/SMG_BBS/repository/entity/Department.java
@@ -1,4 +1,29 @@
 package com.example.SMG_BBS.repository.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
 public class Department {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column(insertable = false, updatable = false)
+    private Timestamp createdDate;
+
+    @Column(insertable = false, updatable = false)
+    private Timestamp updatedDate;
+
 }
