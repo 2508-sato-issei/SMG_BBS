ec1695b2a517740acb29980d179f7e97ec5ba386
shiraiwa_kazuki <shiraiwa.kazuki@alhinc.jp>
2025-09-30 09:32:41 +0900

    レイアウト調整

--- src/main/java/com/example/SMG_BBS/controller/CommentController.java
+++ src/main/java/com/example/SMG_BBS/controller/CommentController.java
@@ -31,8 +31,7 @@ public class CommentController {
                                    @ModelAttribute("formModel") @Validated CommentForm commentForm,
                                    BindingResult result,
                                    @PathVariable Integer messageId,
-                                   RedirectAttributes redirectAttributes,
-                                   HttpSession session){
+                                   RedirectAttributes redirectAttributes){
 
         if(result.hasErrors()){
             String text = commentForm.getText();
@@ -44,6 +43,7 @@ public class CommentController {
             redirectAttributes.addFlashAttribute("comment", text);
             redirectAttributes.addFlashAttribute("commentErrorMessage", errorMessage);
             redirectAttributes.addFlashAttribute("errorId", messageId);
+            redirectAttributes.addFlashAttribute("formModel", commentForm);
             return new ModelAndView("redirect:/");
         }
 
